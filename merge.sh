551e1c4afe662a915aa7ac9b2927bcee32122339
YiM2001 <yma329@wisc.edu>
2022-05-06 11:29:17 -0500

    update

--- merge.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-awk 'FNR==1 && NR!=1{next;}{print}' *.csv > 2009.csv
-
-# https://stackoverflow.com/questions/20275072/read-first-x-lines-of-csv-file-into-new-outfile
--- test_plot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-tar -xzf R402.tar.gz
-
-export PATH=$PWD/R/bin:$PATH
-export RHOME=$PWD/R
-export R_LIBS=$PWD/packages
-
-Rscript plot.R $1
